77dc144bb5dfcc2bd55bd59a60f48213569462ee
Madhurya Dutta <97863234+madhuryadutta@users.noreply.github.com>
2023-12-28 17:03:32 +0530

    Mysql connect completed

---
+++ README.md
@@ -0,0 +1,2 @@
+# geoip 
+#### Project Initiated on 28/12/2023
---
+++ queries.sql
@@ -0,0 +1,9 @@
+-- for create table 
+create table users
+(
+    id     int NOT NULL AUTO_INCREMENT,
+    name   varchar(255),
+    mobile varchar(255),
+    email  varchar(255),
+    PRIMARY KEY (id)
+)
