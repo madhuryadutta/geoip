d12f3108005bce3b93b4c0623414705ab6247dee
Madhurya Dutta <97863234+madhuryadutta@users.noreply.github.com>
2024-03-04 23:57:47 +0530

    simplify main.py && Codestyle Fix

--- main.py
+++ main.py
@@ -1,87 +1,57 @@
 import geoip2.database
 import json
 
-
 # IP address to lookup
-# ip_address = '106.222.227.107'
-# IP address to lookup
-ip_address = '8.8.8.8'  
+ip_address = "104.28.216.196"
 
-# Path to the GeoIP2 database file
-database_path = './db/GeoLite2-City.mmdb'
-
-# Initialize a reader object
-reader = geoip2.database.Reader(database_path)
-
-try:
-    # Perform the lookup
-    response = reader.city(ip_address)
-    # Print all available information
-    print('Country Name:', response.country.name)
-    print('Country ISO Code:', response.country.iso_code)
-    print('Region:', response.subdivisions.most_specific.name)
-    print('City:', response.city.name)
-    print('Latitude:', response.location.latitude)
-    print('Longitude:', response.location.longitude)
-    print('Postal Code:', response.postal.code)
-    print('Time Zone:', response.location.time_zone)
-
-except geoip2.errors.AddressNotFoundError:
-    print('Address not found in the database')
-finally:
-    # Close the reader
-    reader.close()
+# Path to the GeoIP2 City ,Country,ASN database file
+city_database_path = "./db/GeoLite2-City.mmdb"
+country_database_path = "./db/GeoLite2-Country.mmdb"
+asn_database_path = "./db/GeoLite2-ASN.mmdb"
 
 
-# Path to the GeoLite2-Country.mmdb database file
-database_path = './db/GeoLite2-Country.mmdb'
-
-# Initialize a reader object
-reader = geoip2.database.Reader(database_path)
+# Initialize reader object named reader_city ,reader_country ,reader_asn
+reader_city = geoip2.database.Reader(city_database_path)
+reader_country = geoip2.database.Reader(country_database_path)
+reader_asn = geoip2.database.Reader(asn_database_path)
 
 try:
     # Perform the lookup
-    response = reader.country(ip_address)
+    response = reader_city.city(ip_address)
+    print("Country Name:", response.country.name)
+    print("Country ISO Code:", response.country.iso_code)
+    print("Region:", response.subdivisions.most_specific.name)
+    print("City:", response.city.name)
+    print("Latitude:", response.location.latitude)
+    print("Longitude:", response.location.longitude)
+    print("Postal Code:", response.postal.code)
+    print("Time Zone:", response.location.time_zone)
 
-    # Print the results
-    print('Country Name:', response.country.name)
-    print('ISO Code:', response.country.iso_code)
 except geoip2.errors.AddressNotFoundError:
-    print('Address not found in the database')
+    print("Address not found in the database")
 finally:
     # Close the reader
-    reader.close()
+    reader_city.close()
+try:
+    # Perform the lookup
+    response = reader_country.country(ip_address)
 
-
-
-# Path to the GeoLite2-ASN.mmdb database file
-database_path = './db/GeoLite2-ASN.mmdb'
-
-# Initialize a reader object
-reader = geoip2.database.Reader(database_path)
-
-
+    print("Continent:", response.continent.code)
+    print("Continent:", response.continent.names)
+except geoip2.errors.AddressNotFoundError:
+    print("Address not found in the database")
+finally:
+    # Close the reader
+    reader_country.close()
 
 try:
     # Perform the lookup
-    response = reader.asn(ip_address)
+    response = reader_asn.asn(ip_address)
 
-    # Print the results
-    print('ASN:', response.autonomous_system_number)
-    print('ASN Organization:', response.autonomous_system_organization)
+    print("ASN:", response.autonomous_system_number)
+    print("ASN Organization:", response.autonomous_system_organization)
 except geoip2.errors.AddressNotFoundError:
-    print('Address not found in the database')
+    print("Address not found in the database")
 finally:
     # Close the reader
-    reader.close()
-
-
-
-
-
-
-
-
-    # asn_response = reader.asn(ip_address)
-# print('ASN:', asn_response.autonomous_system_number)
-# print('ASN Organization:', asn_response.autonomous_system_organization)
+    reader_asn.close()
--- setup.py
+++ setup.py
@@ -9,62 +9,68 @@ from mysql.connector import Error
 import csv
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
-YOUR_LICENSE_KEY=os.getenv('YOUR_LICENSE_KEY')
-MaxMind_GeoIP_ZIP_NAME=os.getenv('MaxMind_GeoIP_ZIP_NAME')
-Unzipp_Folder='0'
+YOUR_LICENSE_KEY = os.getenv("YOUR_LICENSE_KEY")
+MaxMind_GeoIP_ZIP_NAME = os.getenv("MaxMind_GeoIP_ZIP_NAME")
+Unzipp_Folder = "0"
 
-#Connect DB server and database
+# Connect DB server and database
 conn = mysql.connector.connect(
-  host = os.getenv('DB_HOST'),
-  port = os.getenv('DB_PORT'),
-  user = os.getenv('DB_USERNAME'),
-  password = os.getenv('DB_PASSWORD'),
-  database = os.getenv('DB_DATABASE'),
-  ssl_ca = os.getenv('DB_SSL_CA'),
-  ssl_verify_cert = True,
-  ssl_verify_identity = True
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    user=os.getenv("DB_USERNAME"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_DATABASE"),
+    ssl_ca=os.getenv("DB_SSL_CA"),
+    ssl_verify_cert=True,
+    ssl_verify_identity=True,
 )
 
-def func_download_db(filename,LICENSE_KEY):
-  url="https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City-CSV&license_key="+YOUR_LICENSE_KEY+"&suffix=zip"
-  urlretrieve(url, filename)
 
-def func_unzip_csv(filename,extract_path):
+def func_download_db(filename, LICENSE_KEY):
+    url = (
+        "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City-CSV&license_key="
+        + YOUR_LICENSE_KEY
+        + "&suffix=zip"
+    )
+    urlretrieve(url, filename)
+
+
+def func_unzip_csv(filename, extract_path):
     if os.path.exists(filename):
-        with zipfile.ZipFile(filename, 'r') as zip_ref:
+        with zipfile.ZipFile(filename, "r") as zip_ref:
             zip_ref.extractall(extract_path)
             print("unzip completed")
     else:
         print("The file does not exist")
 
+
 def func_zip_delete(filename):
     if os.path.exists(filename):
-         os.remove(filename)
+        os.remove(filename)
     else:
         print("The file does not exist")
+
 
 def func_find_extract_folder_name():
     global Unzipp_Folder
     path = "./db"
     dir_list = os.listdir(path)
     dir_list.sort()
-    if len(dir_list)>=0 :
-      Unzipp_Folder=dir_list[0]
+    if len(dir_list) >= 0:
+        Unzipp_Folder = dir_list[0]
 
 
-
-
-
-# Download CSV Files from maxmind.com official source using own licence key 
-filename = "db/"+MaxMind_GeoIP_ZIP_NAME
+# Download CSV Files from maxmind.com official source using own licence key
+filename = "db/" + MaxMind_GeoIP_ZIP_NAME
 # func_download_db(filename,YOUR_LICENSE_KEY)
 
 # unziping
-func_unzip_csv(filename,'db/')
+func_unzip_csv(filename, "db/")
 
-# deleting zip file 
+# deleting zip file
 func_zip_delete(filename)
 
 func_find_extract_folder_name()
@@ -72,14 +78,10 @@ func_find_extract_folder_name()
 print(Unzipp_Folder)
 
 
-
-
-
-
-
 cursor = conn.cursor()
 
-cursor.execute('''create table IF NOT EXISTS ipv4
+cursor.execute(
+    """create table IF NOT EXISTS ipv4
 (
     id                             int NOT NULL AUTO_INCREMENT,
     network                        varchar(255),
@@ -93,24 +95,25 @@ cursor.execute('''create table IF NOT EXISTS ipv4
     longitude                      varchar(255),
     accuracy_radius                varchar(255),
     PRIMARY KEY (id)
-)''')
+)"""
+)
 
-ipv4_file='db/'+Unzipp_Folder+"/GeoLite2-City-Blocks-IPv4.csv"
-#open the csv file
-with open(ipv4_file, mode='r') as csv_file:
-    #read csv using reader class
+ipv4_file = "db/" + Unzipp_Folder + "/GeoLite2-City-Blocks-IPv4.csv"
+# open the csv file
+with open(ipv4_file, mode="r") as csv_file:
+    # read csv using reader class
     csv_reader = csv.reader(csv_file)
-    #skip header
+    # skip header
     header = next(csv_reader)
-    #Read csv row wise and insert into table
-    i=1
+    # Read csv row wise and insert into table
+    i = 1
     for row in csv_reader:
-        sql = '''INSERT INTO ipv4 (network, geoname_id, registered_country_geoname_id, represented_country_geoname_id,
+        sql = """INSERT INTO ipv4 (network, geoname_id, registered_country_geoname_id, represented_country_geoname_id,
                   is_anonymous_proxy, is_satellite_provider, postal_code, latitude, longitude, accuracy_radius)
-VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
         cursor.execute(sql, tuple(row))
         print("Record inserted " + str(i))
-        i=i+1;
+        i = i + 1
         conn.commit()
 # conn.commit()
 cursor.close()
